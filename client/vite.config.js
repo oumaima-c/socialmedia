f5fa74049920110a08ae924f640956692e7dce0d
oumaima <oumaimabenloucif3@gmail.com>
2025-08-26 21:27:50 +0100

    np

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 3000
-  },
-  preview: {
-    port: 3000
-  },
-  base: './', // 👈 important for Render so paths work
+  plugins: [react()],
+  base: './',  // 👈 important for static hosting
 })
