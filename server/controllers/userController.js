70eb22887a9e0e594de8d000f86eeffdf3077cc9
oumaima <oumaimabenloucif3@gmail.com>
2025-08-27 20:38:34 +0100

    jk

---
+++ server/controllers/userController.js
@@ -0,0 +1,170 @@
+import { format } from "path"
+import ImageKit from "../configs/imageKit.js"
+import User from "../models/Users.js"
+import fs from 'fs'
+import { use } from "react"
+//Get user data using userId
+export const getUserData=async(req,res)=>{
+    try{
+const {userId}=req.auth()
+const user=await User.findById(userId)
+if(!user){
+    return res.json({success:false,message:"User not found"})
+}res.json({success:true,user})
+    }catch(error){
+console.log(error);
+res.json({success:false,message:error.message})
+    }
+}
+
+
+//Update User Data
+export const updateUserData=async(req,res)=>{
+    try{
+const {userId}=req.auth()
+const {username,bio,location,full_name}=req.body;
+
+
+
+const temperUser=await User.findById(userId)
+
+!username && (username=temperUser.username)
+
+if (temperUser.username !==username){
+    const user=User.findOne({username})
+    if(user){
+        //we will not change the username if the username is already taken
+        username=temperUser.username
+    }
+}
+
+
+
+const updatedData={
+    username,
+    bio,
+    location,
+    full_name
+}
+const profile=req.files.profile &&req.files.profile[0]
+const cover=req.files.cover &&req.files.cover[0]
+if(profile){
+    const buffer=fs.readFileSync(profile.path)
+    const response=await ImageKit.upload({
+        file:buffer,
+        fileName:profile.originalname,
+    })
+    const url=ImageKit.url({
+path:response.filePath,
+transformation:[
+    {quality:'auto'},
+    {format:webp},
+    {width:512}
+]
+
+    })
+    updatedData.profile_picture=url;
+}
+
+
+if(cover){
+    const buffer=fs.readFileSync(cover.path)
+    const response=await ImageKit.upload({
+        file:buffer,
+        fileName:profile.originalname,
+    })
+    const url=ImageKit.url({
+path:response.filePath,
+transformation:[
+    {quality:'auto'},
+    {format:webp},
+    {width:1280}
+]
+
+    })
+    updatedData.cover_picture=url;
+}
+const user=await User.findByIdAndUpdate(userId, updateUserData,{new:true})
+res.json({success:true,user,message:'Profile updated successfully'})
+
+
+
+
+
+    }catch(error){
+console.log(error);
+res.json({success:false,message:error.message})
+    }
+}
+
+
+// find users using username ,email,location
+export const discoverUsers=async(req,res)=>{
+    try{
+const {userId}=req.auth()
+const {input}=req.body;
+const allUsers=await User.find(
+    {
+        $or:[
+            {username:new RegExp(input,'i')},
+            {email:new RegExp(input,'i')},
+            {full_name:new RegExp(input,'i')},
+            {location:new RegExp(input,'i')},
+        ]
+    }
+)
+const filteredUsers=allUsers.filter(user._id !==userId);
+res.json({success:true,users:filteredUsers})
+    }catch(error){
+console.log(error);
+res.json({success:false,message:error.message})
+    }
+}
+
+
+//Follow User
+
+export const followUser=async(req,res)=>{
+    try{
+const {userId}=req.auth()
+const {id}=req.body;
+const user=await User.findById(userId)
+if(user.following.includes(id)){
+    return res.json({success:false,message:'You are already following this user'})
+}
+user.following.push(id);
+await user.save()
+const toUser=await User.findById(id)
+toUser.followers.push(userId)
+await toUser.save()
+res.json({success:true,message:'Now you are following this user'})
+
+    }catch(error){
+console.log(error);
+res.json({success:false,message:error.message})
+    }
+}
+
+
+
+//Unfollow User
+
+export const unfollowUser=async(req,res)=>{
+    try{
+const {userId}=req.auth()
+const {id}=req.body;
+const user=await User.findById(userId)
+user.following=user.following.filter(user=>user !==id);
+await user.save()
+ 
+const toUser=await User.findById(id)
+user.followers=toUser.followers.filter(user=>user!==userId);
+await toUser.save()
+res.json({success:true,message:'You are no longer following this user'})
+
+    }catch(error){
+console.log(error);
+res.json({success:false,message:error.message})
+    }
+}
+
--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import { inngest, functions } from './inngest/index.js';
 import { serve } from 'inngest/express';
+import { clerkMiddleware, ClerkExpressRequireAuth } from "@clerk/express";
+
+import userRouter from './routes/userRotes.js';
+
 
 const app = express();
 await connectDB();
@@ -28,9 +32,12 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use(clerkMiddleware())
+
+
 // ✅ Routes
 app.get('/', (req, res) => res.send('Server is running'));
 app.use('/api/inngest', serve({ client: inngest, functions }));
-
+app.use('/api/user',userRouter)
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`✅ Server is running on port ${PORT}`));
